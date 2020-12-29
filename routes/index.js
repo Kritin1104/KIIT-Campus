1a7a65c31016d44c9dc4ba0b361c8187d4077b84
Kritin Sinha <38904604+KritinSinha@users.noreply.github.com>
2020-12-29 14:56:42 +0530

    Final changes made

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express=require("express");
-var router =express.Router(); //so as we get out of defining app(Version).js in every file 
+var router =express.Router(); 
 var passport=require("passport");
 var User=require("../models/user.js");
 
 //ROOT ROUTE
 router.get("/",function(req,res){
-    // res.send("<h1>Welcome to Home Pageee</h1>");->possible
     res.render("landing.ejs");
 });
 
@@ -22,19 +21,12 @@ router.get("/register",function(req,res){
 router.post("/register",function(req,res){
     var newUser=new User({username:req.body.username});
     User.register(newUser,req.body.password,function(err,user){
-        //===old also works===
         if(err)
         {
             // console.log(err);
             req.flash("error",err.message);
             return res.redirect("/register");
         }
-
-        // //===new does not works====
-        // if(err){
-        //     console.log(err);
-        //     return res.render("register", {error: err.message});
-        // }
 
         passport.authenticate("local")(req,res,function(){
             req.flash("success","Welcome to KIIT Campus "+user.username);
@@ -50,7 +42,7 @@ router.get("/login",function(req,res){
 });
 
 //handle login logic->using middleware-> whole of passport.authenticate part
-//pattern->app7.post("/login",middleware,callback function)
+//pattern->router.post("/login",middleware,callback function)
 router.post("/login",passport.authenticate("local",
     {
         successRedirect:"/campgrounds",
