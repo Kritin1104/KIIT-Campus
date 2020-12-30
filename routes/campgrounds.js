59bbfb3f93547d7c5f620493afa740a37f82c355
Kritin1104 <76537278+Kritin1104@users.noreply.github.com>
2020-12-30 11:46:53 +0530

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,40 +20,6 @@ router.get("/",function(req,res){
     });
     
 });
-
-//CREATE ROUTE(According to RESTful routing)
-//add new campground to DB
-router.post("/",middleware.isLoggedIn,function(req,res){
-    //get data from form and add to campgrounds array
-    var name=req.body.name;
-    var image=req.body.image;
-    var desc=req.body.description; //'description' keyword is coming from the new.ejs file where name="description"
-    var author={
-        id:req.user._id,
-        username:req.user.username
-    }
-    var newCampground={name:name,image:image,description:desc,author:author};
-    //create a new campground & save to DB
-    Campground.create(newCampground,function(err,newlyCreated){
-        if(err)
-        {
-            console.log(err);
-        }
-        else
-        {
-            console.log(newlyCreated);
-            //redirect back to campgrounds page
-            res.redirect("/campgrounds");
-        }
-    });
-
-});
-
-//NEW Route(According to RESTful routing)
-//shows the form where we enter data
-router.get("/new",middleware.isLoggedIn,function(req, res){
-    res.render("campgrounds/new.ejs");
-});
  
 //SHOW - shows more info about one campground
 router.get("/:id",function(req,res){
@@ -70,44 +36,6 @@ router.get("/:id",function(req,res){
             //render show template with that campground
             res.render("campgrounds/show.ejs",{campground:foundCampground});
         }
-    })
-});
-
-//EDIT CAMPGROUND ROUTE
-router.get("/:id/edit",middleware.checkCampgroundOwnership,function(req,res){
-    Campground.findById(req.params.id,function(err,foundCampground){
-        res.render("campgrounds/edit.ejs",{campground:foundCampground});
-    })
-});
-
-//UPDATE CAMPGROUND ROUTE for edit
-router.put("/:id",middleware.checkCampgroundOwnership,function(req,res){
-    //find and update the correct campground
-    Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCampground){
-             if(err)
-             {
-                 console.log(err);
-                 res.redirect("/campgrounds");
-             }
-             else
-             {
-                 res.redirect("/campgrounds/" + req.params.id);
-             }
-    })
-    //redirect somewhere(show page for the updated campground)
-});
-
-//DESTROY Campground route for edit
-router.delete("/:id",middleware.checkCampgroundOwnership,function(req,res){
-    Campground.findByIdAndRemove(req.params.id,function(err){
-        if(err)
-        {
-            res.redirect("/campgrounds");
-        } 
-        else
-        {
-            res.redirect("/campgrounds")
-        }  
     })
 });
 
